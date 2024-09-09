32f7258657e050f9ec6615406225ee1824b1caad
dptrek <dptrek@proton.me>
2024-09-09 15:20:38 -0400

    Add files via upload

---
+++ DP_website/CgTest/Forced-2/script.js
@@ -0,0 +1,14 @@
+const checkbox = document.getElementById('agree-checkbox');
+const agreebutton = document.getElementById('continue-button-1');
+
+checkbox.addEventListener('change', function() {
+    recordAction(2, "checkbox");
+    agreebutton.disabled = !this.checked;
+});
+
+agreebutton.addEventListener('click',function(){
+    recordAction(1, "continue and agree");
+    code_score_control(2);
+    window.open("../../Control/Forced/trans_test_2.html","_self");
+});
+
